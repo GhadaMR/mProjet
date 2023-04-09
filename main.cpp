62a31ffc0053b20df433c4e65e23825b1a884344
Maram Zribi <129750651+Cacao8@users.noreply.github.com>
2023-04-09 01:02:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,42 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <ctime>
 #include <string>
+#include <vector>
 #include <sstream>
 using namespace std;
 class Date{
+
      int jour;
      int mois;
      int annee;
+
     public:
-     string incrimenterDate(string);
+     vector<int>v30={1,3,5,7,8,10,12};
+     vector<int> v31={4,6,9,11};
+     string incrimenterDate(Date);
      string afficherDate ();
      void setJour(int j);
      void setMois(int m);
      void setAnnee(int a);
      Date (string d);
      int getJour();
+     int getMois();
+     int getAnnee();
+
 };
 int Date::getJour()
 {
     return jour;
 }
-string Date:: incrimenterDate(string)
+int Date::getMois()
 {
-
+    return mois;
 }
+int Date::getAnnee()
+{
+    return annee;
+}
+
 void Date::setMois(int m)
 {
  mois++;
@@ -36,6 +49,53 @@ void Date::setAnnee(int a)
 {
  annee++;
 }
+string Date::incrimenterDate(Date d) {
+    if (mois != 12) {
+        if (jour == 30) {
+            if (find(v30.begin(),v30.end(),jour) != v30.end()) {
+                d.setMois(mois);
+                jour=1;
+            }
+        }
+        else if (find(v31.begin(), v31.end(), mois) != v31.end()) {
+            d.setJour(jour );
+        }
+        else {
+            d.setJour(jour);
+        }
+    }
+    else if (mois==2){
+            if(jour==28){
+                if (annee %4==0)
+                        d.setJour(28);
+
+                else{
+                    this->jour=1;
+                    this->mois=3;
+
+                }
+
+            }
+          else
+            d.setJour(jour);
+    }
+   else{
+     if (jour==30){
+            mois=1;
+            jour=1;
+            annee+=1;
+            }
+    else
+        d.setJour(jour);
+   }
+
+
+    string d1 = to_string(d.getJour()) + "/" + to_string(d.getMois()) + "/" + to_string(d.getAnnee());
+
+    return d1;
+}
+
+
 Date::Date(string d){
     istringstream ss(d);
     string j, m, a;
@@ -54,9 +114,10 @@ Date::Date(string d){
 
 int main()
 {
-    Date d("1/02/2022");
-    cout<< d.getJour()<<endl;
-  
-     
+    Date d("28/2/2022");
+    cout<<d.incrimenterDate(d) <<endl;
+
+
     return 0;
 }
+
